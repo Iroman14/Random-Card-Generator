20c711d148e3985afb092ae8d82ed17934ee3c98
Iroman14 <134985621+Iroman14@users.noreply.github.com>
2025-01-03 21:33:38 +0000

    Final

--- src/app.js
+++ src/app.js
@@ -5,7 +5,47 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const button = document.querySelector("#Execute");
+
+button.addEventListener("click", card);
+
+function card() {
+  let CardNum = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let Symbols = ["Hearts", "Clubs", "Diamonds", "Spades"];
+  let CardNumber = Math.floor(Math.random() * CardNum.length);
+  let Symb = Math.floor(Math.random() * Symbols.length);
+  let FinSym = Symbols[Symb];
+
+  document.getElementById("Number").innerHTML = CardNum[CardNumber];
+  document.getElementById("Symbol").className = "";
+  document.getElementById("Symbol").classList.add(getSymbol(FinSym));
+
+  function getSymbol(Symbols) {
+    switch (Symbols) {
+      case "Hearts":
+        return "heart";
+      case "Clubs":
+        return "club";
+      case "Diamonds":
+        return "diamond";
+      case "Spades":
+        return "spade";
+    }
+  }
+}
+
+setInterval(card, 10000);
