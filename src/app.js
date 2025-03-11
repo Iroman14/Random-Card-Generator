c28abe4bb8a78965d9e184465c4d6db9d1ecd584
Iroman14 <134985621+Iroman14@users.noreply.github.com>
2025-03-11 02:19:22 +0000

    Proyecto Mejorado por el Coding Guidelines Challenge

--- src/app.js
+++ src/app.js
@@ -7,10 +7,12 @@ import "./assets/img/4geeks.ico";
 
 const button = document.querySelector("#Execute");
 
-button.addEventListener("click", card);
+button.addEventListener("click", generateCard);
 
-function card() {
-  let CardNum = [
+function generateCard() {
+  /*Se cambio el nombre de card a generateCard*/
+  const cardNumbers = [
+    /*Se cambio de let a const para mejorar la inicializacion de la variable y se cambio el nombre de la variable de cardnum a cardnumbers para una mejor lectura*/
     "A",
     "2",
     "3",
@@ -25,17 +27,35 @@ function card() {
     "Q",
     "K"
   ];
-  let Symbols = ["Hearts", "Clubs", "Diamonds", "Spades"];
-  let CardNumber = Math.floor(Math.random() * CardNum.length);
-  let Symb = Math.floor(Math.random() * Symbols.length);
-  let FinSym = Symbols[Symb];
 
-  document.getElementById("Number").innerHTML = CardNum[CardNumber];
-  document.getElementById("Symbol").className = "";
-  document.getElementById("Symbol").classList.add(getSymbol(FinSym));
+  const cardSuits = [
+    "Hearts",
+    "Clubs",
+    "Diamonds",
+    "Spades"
+  ]; /*Se cambio de let a const para mejorar la inicializacion de la variable y se cambio el nombre de la variable symbols a cardSuits para una mejor lectura*/
 
-  function getSymbol(Symbols) {
-    switch (Symbols) {
+  const randomNumber = Math.floor(
+    Math.random() * cardNumbers.length
+  ); /*Se cambio de let a const para mejorar la inicializacion de la variable y se cambio el nombre de la variable de cardnumber a randomNumber para una mejor lectura*/
+
+  const randomSuit = Math.floor(
+    Math.random() * cardSuits.length
+  ); /* Se cambio de let a const para mejorar la inicializacion de la variable*/
+
+  const finalSuit =
+    cardSuits[
+      randomSuit
+    ]; /*Se cambio de let a const para mejorar la inicializacion de la variable y se cambio el nombre de la variable de FinSym a finalSuit para una mejor lectura*/
+
+  document.getElementById("Number").innerHTML = cardNumbers[randomNumber];
+  document.getElementById("Suit").className = "";
+  document.getElementById("Suit").classList.add(getSuit(finalSuit));
+
+  function getSuit(Suits) {
+    /*Se cambio el nombre de la funcion a getSuit para una mejor lectura*/
+
+    switch (Suits) {
       case "Hearts":
         return "heart";
       case "Clubs":
@@ -48,4 +68,4 @@ function card() {
   }
 }
 
-setInterval(card, 10000);
+setInterval(generateCard, 10000);
